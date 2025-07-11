adfdc2e9dc314fd18d8458fbb3521011d8055eca
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2025-07-11 01:57:38 -0500

    solve ch 2 exercises

--- chapter-02/exercises.js
+++ chapter-02/exercises.js
@@ -26,8 +26,16 @@ LOGS =>
 
 */
 
-function triangles() {
-  
+function triangles(size) {
+	//string to hold the number of #'s
+	let line = "";
+	//loop the number of times specified by the input
+	for (let i = 0; i < size; i++) {
+		//lengthen the line to be printed
+		line += "#";
+		//print the line
+		console.log(line);
+	}	
 }
 
 
@@ -41,14 +49,29 @@ Directions: Create a function called fizzBuzz that takes in two parameters -
 access each number from `start` to `end` and log different statements depending
 on the number:
 
-  - if the number is divisible by 3, log "fizz"
-  - if the number is divisible by 5, log "buzz"
-  - if the number is divisible by both 3 & 5, log "fizzbuzz"
-  - if the number is not divisible by 3 or 5, log the number
+	- if the number is divisible by 3, log "fizz"
+	- if the number is divisible by 5, log "buzz"
+	- if the number is divisible by both 3 & 5, log "fizzbuzz"
+	- if the number is not divisible by 3 or 5, log the number
 */
 
 function fizzBuzz(start, end) {
-  
+	//loop from start to end (assuming start < end)
+	for (let i = start; i <= end; i++) {
+		//initialize empty string to be printed if the number is divisible by either 3 or 5
+		let string = "";
+		//add fizz to string if divisible by three
+		if (i % 3 === 0) {
+			string += "fizz";
+		}
+		//add buzz to string if divisible by five
+		if (i % 5 === 0) {
+			string += "buzz";
+		}
+		//because these are independent ifs rather than else-ifs, string will be "fizzbuzz" if the number is divisible by both 3 and five
+		//log the string if it is nonempty, the number if it is
+		console.log(string ? string : i);
+	}	
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -82,8 +105,25 @@ LOGS =>
 */
 
 function drawChessboard(x) {
+	//initialize empty string to build the chessboard on
+	let chessboard = "";
 
+	//loop x times to create each row
+	for (let i = 0; i < x; i++) {
+		//loop x times to create square within each row
+		for (let j = 0; j < x; j++) {
+			//decide which character to append
+			//an even sum of row and column indices gets a space, an odd gets a #
+			//this means that moving along any straight line alternates black and white squares
+			chessboard += (i + j) % 2 === 0 ? " " : "#";
+		}
+		//add a new line after every row
+		//it's kind of weird that you need the trailing newline to pass the tests
+		chessboard += "\n";
+	}
 
+	//log the assembled board
+	console.log(chessboard);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -91,10 +131,10 @@ function drawChessboard(x) {
 ////////////////////////////////////////////////////////////////////////////////
 
 if ((typeof process !== 'undefined') &&
-  (typeof process.versions.node !== 'undefined')) {
-  module.exports = {
-    triangles,
-    fizzBuzz,
-    drawChessboard,
-  };
+	(typeof process.versions.node !== 'undefined')) {
+	module.exports = {
+		triangles,
+		fizzBuzz,
+		drawChessboard,
+	};
 }
